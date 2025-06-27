867517a85667ae985aff5c7a1a64b92a91588779
denysmatvieiev <matveevdenis458@gmail.com>
2025-06-27 11:27:04 +0300

    feat(Search): add search section

--- js/main.js
+++ js/main.js
@@ -11,11 +11,11 @@ pickupButton.addEventListener("click", () => {
   deliveryButton.classList.remove("active");
 });
 
-const slidesPerView = () => {
+const slidesPerView = (initialValue) => {
   const width = window.innerWidth;
 
   if (width >= 1100) {
-    return 5;
+    return initialValue;
   }
 
   if (width < 1100 && width >= 768) {
@@ -27,12 +27,21 @@ const slidesPerView = () => {
   }
 };
 
-const swiper = new Swiper(".mySwiper", {
-  slidesPerView: slidesPerView(),
+const swiper = new Swiper(".mySwiper-popular", {
+  slidesPerView: slidesPerView(5),
   spaceBetween: 16,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next-popular",
+    prevEl: ".swiper-button-prev-popular",
+  },
+});
+
+const swiperSearchSection = new Swiper(".mySwiper-search", {
+  slidesPerView: slidesPerView(6),
+  spaceBetween: 34,
+  navigation: {
+    nextEl: ".swiper-button-next-search",
+    prevEl: ".swiper-button-prev-search",
   },
 });
 
@@ -42,15 +51,24 @@ window.addEventListener("resize", () => {
   if (width >= 1100) {
     swiper.params.slidesPerView = 5;
     swiper.update();
+
+    swiperSearchSection.params.slidesPerView = 5;
+    swiperSearchSection.update();
   }
 
   if (width < 1100 && width >= 768) {
     swiper.params.slidesPerView = 3;
     swiper.update();
+
+    swiperSearchSection.params.slidesPerView = 3;
+    swiperSearchSection.update();
   }
 
   if (width < 768) {
     swiper.params.slidesPerView = 1;
     swiper.update();
+
+    swiperSearchSection.params.slidesPerView = 1;
+    swiperSearchSection.update();
   }
 });
